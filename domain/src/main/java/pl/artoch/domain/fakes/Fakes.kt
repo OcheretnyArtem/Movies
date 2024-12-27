0e9ae5ccee6b3b670c457fa0355bb14ce85cc443
artsiomacharetny <ocheretnyartem@gmail.com>
2024-12-27 17:28:37 +0100

    Add valid data for imageUrl field in movie's fake

--- domain/src/main/java/pl/artoch/domain/fakes/Fakes.kt
+++ domain/src/main/java/pl/artoch/domain/fakes/Fakes.kt
@@ -6,5 +6,5 @@ fun fakeMovie(
     id: String = "1",
     name: String = "Fake Movie $id",
     description: String? = "Fake description",
-    imageUrl: String? = null
+    imageUrl: String? = "https://image.tmdb.org/t/p/w500/cdqLnri3NEGcmfnqwk2TSIYtddg.jpg"
 ) = Movie(id = Movie.Id(id), name = name, description = description, imageUrl = imageUrl)
